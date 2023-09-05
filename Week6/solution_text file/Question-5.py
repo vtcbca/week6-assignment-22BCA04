d6ce5ddd8f1653c662c24dd5c8b16bd35622bf40
Vishal Agrawal <122864330+22BCA04@users.noreply.github.com>
2023-09-05 18:06:31 +0530

    Update Question-5.py

--- Week6/solution_text file/Question-5.py
+++ Week6/solution_text file/Question-5.py
@@ -4,7 +4,7 @@ upper_file="c://sqlite3//python_upper.txt"
 
 with open(filename,"r")as read_file,open(upper_file,"w")as write_file:
     read=read_file.read()
-    #content of python.txt file is save in read and it is convert into list
+    #content of python.txt file is save in read
     for i in read:
         data2=i.upper()
         write_file.write(data2)
